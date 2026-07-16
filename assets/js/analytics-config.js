16f6032d8dd7cac600c3918ed68fbe2849d2070f
valiharabemananjara-code <valiharabemananjara@gmail.com>
2026-07-16 07:48:13 +0300

    Add files via upload

---
+++ assets/js/analytics-config.js
@@ -0,0 +1,8 @@
+/*
+  Ajoutez ici vos identifiants après création des comptes.
+  Le suivi reste désactivé tant que les valeurs XXXX ne sont pas remplacées.
+*/
+window.KAIROS_ANALYTICS = {
+  googleAnalyticsId: "G-XXXXXXXXXX",
+  microsoftClarityId: "XXXXXXXXXX"
+};
